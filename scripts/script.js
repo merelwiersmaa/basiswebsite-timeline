1271902cecca60979909cd3ec54f67659593d9b3
merelwiersmaa <merelwiersma@outlook.com>
2023-04-24 11:13:36 +0200

    dialog eerste begin

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
 // JavaScript Document
 console.log("heeey!");
+
+////////////////////////////////////////////////////////////////////////////////
+// OPEN/CLOSE THE LIME, Bron van Sanne: https://codepen.io/shooft/pen/wvYgVKg///
+////////////////////////////////////////////////////////////////////////////////
+
+const limeOpenButton = document.querySelector("ul li:nth-of-type(1) > button");
+const limeDialog = document.querySelector("ul li:nth-of-type(1) dialog");
+
+limeOpenButton.addEventListener("click", openLimeDialog);
+
+function openLimeDialog() {
+	limeDialog.showModal();
+}
