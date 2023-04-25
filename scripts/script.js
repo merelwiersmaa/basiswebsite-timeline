2f09319bf202dc05d6ecaebd44de1780cd87f495
merelwiersmaa <merelwiersma@outlook.com>
2023-04-25 09:00:14 +0200

    dialog delay

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,13 @@ console.log("heeey!");
 const limeOpenButton = document.querySelector("ul li:nth-of-type(1) > button");
 const limeDialog = document.querySelector("ul li:nth-of-type(1) dialog");
 
-limeOpenButton.addEventListener("click", openLimeDialog);
 
-function openLimeDialog() {
-	limeDialog.showModal();
+limeOpenButton.addEventListener("click", openDialog);
+
+function openDialog() {
+    setTimeout(() => {
+        limeDialog.showModal();
+    }, 500);
 }
 
 
